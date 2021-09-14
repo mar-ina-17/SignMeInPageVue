b8abe62eb2a4c12b9e5cbe4622b1612712db977c
mar-ina-17 <marina.workplace@gmail.com>
2021-09-14 13:24:33 +0300

    Full landing page for sign up bulletin w/o the backend but with localization

---
+++ src/LandingPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h3 v-html="$t(`landingPage.header`)"></h3>
+    <signMeInBox/>
+  </div>
+</template>
+
+<script>
+import signMeInBox from './components/signMeInBoxComponent.vue'
+
+export default {
+  name: 'LandingPage',
+  components: {
+    signMeInBox
+  }
+}
+</script>
+
+<style>
+@import './styles/landingPageStyle.css';
+</style>
---
+++ src/components/signMeInBoxComponent.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="input-box">
+    <span id="span-box-label" v-html="$t(`signMeInComponent.inputMailLabel`)"></span>
+    <input v-model="userInput" type="text" @input="isInputValid(userInput)" id="sign-in-box"><br>
+    <span id = "error-msg-span">{{incBoolean ?  " " : $t(`signMeInComponent.errorMessage`)}}</span>
+    <div class="send-button">
+      <button type="submit" @click="signMeIn()" id="sign-in-button" v-html="$t(`signMeInComponent.signMeButton`)"></button>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default 
+{
+  name: 'signMeInBoxComponent',
+
+  data() 
+  {
+    return {
+      userInput: " ",
+      incBoolean: true,
+    }
+    
+  },
+
+  methods: 
+  {
+    isInputValid(userInput)
+    {
+      this.incBoolean = userInput.includes("@");
+      console.log(this.incBoolean);
+    },
+
+    signMeIn()
+    {
+      if(this.isInputValid(this.userInput)) 
+      {
+        console.log("signed in");
+      }
+      else
+      {
+        console.log("sorry, not signed in");
+      }
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ @import '../styles/signMeInBoxComponentStyle.css';
+</style>
